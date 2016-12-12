5053af1fb6da107c1b5b5e24349579d3e4f8cee0
Mark Bailey <pigdogbay@yahoo.co.uk>
2016-12-12 15:25:33 +0000

    Colors

--- app/src/main/java/com/pigdogbay/codewordsolver/usercontrols/LetterPickerDialog.java
+++ app/src/main/java/com/pigdogbay/codewordsolver/usercontrols/LetterPickerDialog.java
@@ -34,7 +34,6 @@ public class LetterPickerDialog implements View.OnClickListener {
         View lettersView = View.inflate(context, R.layout.letter_picker,null);
         setUpButtons(lettersView);
 
-        int s = R.style.AppCompatAlertDialogStyle;
         AlertDialog.Builder builder =
                 new AlertDialog.Builder(context, R.style.letter_picker_dialog);
         builder.setTitle("Select Letter for "+square.getNumberString());
